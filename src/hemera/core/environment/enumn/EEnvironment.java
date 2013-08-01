6eb61a8422f6bdb545daa64dcd3bef983a3488e8
Yi Wang <neakor@gmail.com>
2013-07-31 23:09:07 -0400

    Version number.

--- src/hemera/core/environment/enumn/EEnvironment.java
+++ src/hemera/core/environment/enumn/EEnvironment.java
@@ -5,13 +5,13 @@ package hemera.core.environment.enumn;
  * of various system values used.
  *
  * @author Yi Wang (Neakor)
- * @version 1.0.5
+ * @version 1.0.6
  */
 public enum EEnvironment {
 	/**
 	 * The current version number.
 	 */
-	Version("1.0.5"),
+	Version("1.0.6"),
 	/**
 	 * The configuration XML file name under the home bin
 	 * directory.
